2d847ffa2f11c684ce01378cfbad857de1ccb336
kgreufe <kgreufe@gmail.com>
2021-01-18 17:05:39 -0700

    create file

---
+++ 21-0112_Art_Collections/21-0112_tidy_tuesday_art_collections.R
@@ -0,0 +1,22 @@
+library(tidytuesdayR)
+library(tidyverse)
+library(extrafont)
+
+
+# LOAD DATA ---------------------------------------------------------------
+
+tt <- tidytuesdayR::tt_load("2021", week = 3)
+
+artwork <- tt$artwork
+artists <- tt$artists
+
+
+
+
+# EXPLORE -----------------------------------------------------------------
+
+
+
+
+# PLOT -------------------------------------------------------------
+
